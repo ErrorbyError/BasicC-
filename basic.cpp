4f1476ceb06122b3f1526473b136173535b002ba
Pedro <talissoncarmo65@gmail.com>
2023-11-18 09:00:57 -0300

    firstDay

---
+++ basic.cpp
@@ -0,0 +1,27 @@
+// iosStream hold inputs and outputs operations.. and a little more.
+#include <iostream>
+
+int main(){
+    // std means standard.
+    // cout >> the "c" means character, and the fallowing "out" means output. 
+    // standard character output.      endl means endLine. 
+    std::cout << "I like Pudim!" << std::endl;
+    std::cout << "It's too good to be true" << std::endl;
+
+    // there's another way to separate the lines. it is by using: '\n'  
+
+    std::cout << "I will achieve everything." << '\n';
+    std::cout << "I'll make sure about it." << '\n';
+
+
+
+
+
+
+
+
+
+    //the use of return 0:  it is because if the program return 0, there's nothing wrong with our program, but if it return 1. there's a problem in it. 
+    return 0;
+}
+
---
+++ variables.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main(int argc, const char** argv) {
+    
+    int x; // declaration
+    int y; 
+    x = 5;  // assignment 
+    y = 6;
+// types
+    // int
+    int age = 21; // int means integer which is a whole number. 
+    // what would happen if I use a double value into a int declaration? 
+
+    int my_age = 21.9;
+
+    std::cout << my_age << '\n';
+    // the value before the colon is ignored 
+    std::cout << x; 
+
+    
+    return 0;
+}
